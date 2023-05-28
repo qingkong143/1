83aa97305919a9c858824fe92d5d991591948d93
qingkong143 <3186708635@qq.com>
2023-05-28 16:44:22 +0800

    Site updated: 2023-05-28 16:44:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/26/更新啦/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/05/26/更新啦/","2023/05/28/test/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
