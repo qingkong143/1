75a3ee77e2c5ec681a96c9ec8b058ed8bc987adc
qingkong143 <3186708635@qq.com>
2023-06-09 09:21:51 +0800

    Site updated: 2023-06-09 09:21:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/06/07/Windows系统推荐和安装，简单的优化和美化以及一些软件分享/","2023/06/08/privacy-policy/","2023/05/28/test/","2023/06/08/导数的简单教学/","2023/05/26/更新啦/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/06/07/Windows系统推荐和安装，简单的优化和美化以及一些软件分享/","2023/06/08/privacy-policy/","2023/05/28/test/","2023/06/08/导数的简单教学/","2023/05/26/更新啦/","2023/06/09/爬取某4K壁纸网站图片/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
