9b1c614d7a71dda2c3284e689f1f0d2f3a303456
qingkong143 <3186708635@qq.com>
2023-07-07 10:25:58 +0800

    Site updated: 2023-07-07 10:25:58

---
+++ js/date.js
@@ -0,0 +1,26 @@
+
+/* */
+if (PublicSacrificeDay()) {
+  document.getElementsByTagName("html")[0].setAttribute("style", "filter:gray !important;filter:grayscale(100%);-webkit-filter:grayscale(100%);-moz-filter:grayscale(100%);-ms-filter:grayscale(100%);-o-filter:grayscale(100%);");
+}
+function PublicSacrificeDay() {
+  var PSFarr = new Array("0404", "0405", "0414", "0512", "0707", "0807", "0814", "0909", "0918", "0930", "1025", "1130", "1213");
+  var currentdate = new Date();
+  var str = "";
+  var mm = currentdate.getMonth() + 1;
+  if (currentdate.getMonth() > 9) {
+    str += mm;
+  } else {
+    str += "0" + mm;
+  }
+  if (currentdate.getDate() > 9) {
+    str += currentdate.getDate();
+  } else {
+    str += "0" + currentdate.getDate();
+  }
+  if (PSFarr.indexOf(str) > -1) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
